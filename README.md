892cf5b13454e5e17036b642a74139d5b2a1bd74
EinMilchBoss <christian.benezuela@gmail.com>
2023-04-20 16:30:27 +0200

    Setup Prettier.

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 Just some Advent of Code to get familiar with TypeScript.
 
 Join Advent of Code yourself: https://adventofcode.com/.
+
+# Useful scripts
+
+## Checking for Pretter ESlint collisions
+
+```bash
+npx eslint-config-prettier <file-name>
+```
--- src/util.ts
+++ src/util.ts
@@ -1,5 +1,9 @@
 import path from 'node:path';
 
-export function getResource(year: string, day: string, fileName: string): string {
-    return path.join(__dirname, '..', 'res', year, day, fileName);
+export function getResource(
+	year: string,
+	day: string,
+	fileName: string
+): string {
+	return path.join(__dirname, '..', 'res', year, day, fileName);
 }
