52c1d1aafc6bb3ea0061d7bb804b4f437c7635e3
EinMilchBoss <christian.benezuela@gmail.com>
2023-04-20 16:31:59 +0200

    Edit tabs.

--- src/2015/05/main.ts
+++ src/2015/05/main.ts
@@ -3,58 +3,58 @@ import fs from 'node:fs';
 import * as util from '../../util';
 
 export function countVowels(line: string): number {
-	const vowels = ['a', 'i', 'u', 'e', 'o'];
-	let vowelCount = 0;
-	line.split('').forEach((char) => {
-		if (vowels.includes(char)) {
-			++vowelCount;
-		}
-	});
-	return vowelCount;
+    const vowels = ['a', 'i', 'u', 'e', 'o'];
+    let vowelCount = 0;
+    line.split('').forEach((char) => {
+        if (vowels.includes(char)) {
+            ++vowelCount;
+        }
+    });
+    return vowelCount;
 }
 
 export function hasLetterAdjacent(line: string, minTimes = 2): boolean {
-	for (let i = 0; i <= line.length - minTimes; ++i) {
-		const window = line.slice(i, i + minTimes);
-		if (window[0] === window[1]) {
-			return true;
-		}
-	}
-	return false;
+    for (let i = 0; i <= line.length - minTimes; ++i) {
+        const window = line.slice(i, i + minTimes);
+        if (window[0] === window[1]) {
+            return true;
+        }
+    }
+    return false;
 }
 
 export function includesSome(line: string, parts: string[]): boolean {
-	return parts.some((part) => line.includes(part));
+    return parts.some((part) => line.includes(part));
 }
 
 export function solveFirst(input: string): number {
-	const naughtyParts = ['ab', 'cd', 'pq', 'xy'];
+    const naughtyParts = ['ab', 'cd', 'pq', 'xy'];
 
-	let niceStringCount = 0;
-	input.split('\n').forEach((line) => {
-		if (
-			countVowels(line) >= 3 &&
-			hasLetterAdjacent(line) &&
-			!includesSome(line, naughtyParts)
-		) {
-			++niceStringCount;
-		}
-	});
+    let niceStringCount = 0;
+    input.split('\n').forEach((line) => {
+        if (
+            countVowels(line) >= 3 &&
+            hasLetterAdjacent(line) &&
+            !includesSome(line, naughtyParts)
+        ) {
+            ++niceStringCount;
+        }
+    });
 
-	return niceStringCount;
+    return niceStringCount;
 }
 
 export function solveSecond(_: string): number {
-	return 0;
+    return 0;
 }
 
 function main() {
-	const input = fs
-		.readFileSync(util.getResource('2015', '05', 'input.txt'), 'utf8')
-		.toString();
+    const input = fs
+        .readFileSync(util.getResource('2015', '05', 'input.txt'), 'utf8')
+        .toString();
 
-	console.log(`First: ${solveFirst(input)}`);
-	console.log(`Second: ${solveSecond(input)}`);
+    console.log(`First: ${solveFirst(input)}`);
+    console.log(`Second: ${solveSecond(input)}`);
 }
 
 main();
--- src/util.ts
+++ src/util.ts
@@ -1,9 +1,9 @@
 import path from 'node:path';
 
 export function getResource(
-	year: string,
-	day: string,
-	fileName: string
+    year: string,
+    day: string,
+    fileName: string
 ): string {
-	return path.join(__dirname, '..', 'res', year, day, fileName);
+    return path.join(__dirname, '..', 'res', year, day, fileName);
 }
