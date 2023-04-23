f7e8d0596eb162da0d601544879ca3fd6ebc270d
EinMilchBoss <christian.benezuela@gmail.com>
2023-04-23 21:37:48 +0200

    Remove warnings.

--- test/2015/05/main.spec.ts
+++ test/2015/05/main.spec.ts
@@ -1,18 +1,8 @@
 import assert from 'node:assert';
-import fs from 'node:fs';
 
 import * as uut from '../../../src/2015/05/main';
-import * as util from '../../../src/util';
 
 suite('2015-05-main', () => {
-    let example: string;
-
-    suiteSetup(() => {
-        example = fs
-            .readFileSync(util.getResource('2015', '05', 'example.txt'), 'utf8')
-            .toString();
-    });
-
     test('countVowels', () => {
         assert.strictEqual(uut.countVowels('aiueo'), 5);
         assert.strictEqual(uut.countVowels('a123a'), 2);
